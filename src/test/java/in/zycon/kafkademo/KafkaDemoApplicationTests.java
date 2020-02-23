efa39bc46d04aa44a28fbf8d8214ffec5289ba14
Tibin Lukose <tibin@zycon.in>
2020-02-23 12:27:26 +0400

    initial

---
+++ src/test/java/in/zycon/kafkademo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.zycon.kafkademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
